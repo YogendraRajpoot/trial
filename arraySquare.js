4f644800c1760697eaf9b74306ba8c6d19df4d75
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-01-13 00:53:55 +0530

    first commit

---
+++ arraySquare.js
@@ -0,0 +1,8 @@
+// Given an array of numbers generate an 
+// array containing the square value of the numbers
+
+var arr = [2,3,5,7,9]
+const arrayDouble = x => x=x*x
+
+console.log(arr.map(arrayDouble))
+
